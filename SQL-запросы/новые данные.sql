b025d13e298bc76d56bde451532399d67c80edeb
tanks8015-hue <tanks8015@gmail.com>
2026-04-02 13:02:49 +0400

    добавлены новые тестовые данные в бд

---
+++ SQL-запросы/новые данные.sql
@@ -0,0 +1,3 @@
+INSERT INTO Products (ProductName, BasePrice) VALUES ('Двигатель А-100', 150000.00);
+INSERT INTO Products (ProductName, BasePrice) VALUES ('Шестерня вала', 2500.50);
+INSERT INTO Products (ProductName, BasePrice) VALUES ('Корпус металлический', 45000.00);
