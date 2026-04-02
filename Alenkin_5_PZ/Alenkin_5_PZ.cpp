06eb8560913f4f4350525b4d5313622609934d20
tanks8015-hue <tanks8015@gmail.com>
2026-04-02 12:59:50 +0400

    исправление главного Дашбокса

--- Alenkin_5_PZ/Alenkin_5_PZ.cpp
+++ Alenkin_5_PZ/Alenkin_5_PZ.cpp
@@ -1,127 +1,52 @@
-﻿#include "framework.h"
+﻿// Alenkin_5_PZ.cpp
+#include "framework.h"
 #include "Alenkin_5_PZ.h"
-#define IDC_USERNAME_EDIT 101
-#define IDC_PASSWORD_EDIT 102
-#define IDC_LOGIN_BUTTON 103
-#define MAX_LOADSTRING 100
 #include <wincrypt.h> 
-#include <sql.h>     
-#include <sqlext.h>  
+#include <sql.h>      
+#include <sqlext.h>   
 #include <string>
 
 #pragma comment(lib, "advapi32.lib")
-HINSTANCE hInst;    
-WCHAR szDashboardClass[] = L"DashboardWindow";
+
+#define MAX_LOADSTRING 100
+
+// ID для элементов интерфейса
+#define IDC_USERNAME_EDIT 101
+#define IDC_PASSWORD_EDIT 102
+#define IDC_LOGIN_BUTTON 103
+
+// Глобальные переменные:
+HINSTANCE hInst;
 WCHAR szTitle[MAX_LOADSTRING];
 WCHAR szWindowClass[MAX_LOADSTRING];
+WCHAR szDashboardClass[] = L"DashboardWindow";
+
+// АНОНСЫ (Объявления функций):
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK    DashboardWndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_ALENKIN5PZ, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // Выполнить инициализацию приложения:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ALENKIN5PZ));
-
-    MSG msg;
-    // Регистрация класса окна Дашборда
-    WNDCLASSEXW wcexDash;
-    wcexDash.cbSize = sizeof(WNDCLASSEX);
-    wcexDash.style = CS_HREDRAW | CS_VREDRAW;
-    wcexDash.lpfnWndProc = DashboardWndProc;
-    wcexDash.cbClsExtra = 0;
-    wcexDash.cbWndExtra = 0;
-    wcexDash.hInstance = hInstance;
-    wcexDash.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ALENKIN5PZ));
-    wcexDash.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcexDash.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcexDash.lpszMenuName = NULL;
-    wcexDash.lpszClassName = szDashboardClass;
-    wcexDash.hIconSm = LoadIcon(wcexDash.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-    RegisterClassExW(&wcexDash);
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
-}
-
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ALENKIN5PZ));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_ALENKIN5PZ);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
+// Функция для генерации SHA-256 хэша
 std::wstring GenerateSHA256(const std::wstring& input)
 {
-    // 1. Конвертируем wstring (UTF-16) в string (UTF-8)
-    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &input[0], (int)input.size(), NULL, 0, NULL, NULL);
+    // Броня от пустого пароля (чтобы не было вылета)
+    if (input.empty()) return L"";
+
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, input.c_str(), (int)input.length(), NULL, 0, NULL, NULL);
     std::string utf8_input(size_needed, 0);
-    WideCharToMultiByte(CP_UTF8, 0, &input[0], (int)input.size(), &utf8_input[0], size_needed, NULL, NULL);
+    WideCharToMultiByte(CP_UTF8, 0, input.c_str(), (int)input.length(), &utf8_input[0], size_needed, NULL, NULL);
 
     HCRYPTPROV hProv = 0;
     HCRYPTHASH hHash = 0;
     std::wstring hashStr = L"";
 
-    // 2. Хэшируем
     if (CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
     {
         if (CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash))
         {
-            // Скармливаем нормальную UTF-8 строку!
-            if (CryptHashData(hHash, (const BYTE*)utf8_input.c_str(), utf8_input.length(), 0))
+            if (CryptHashData(hHash, (const BYTE*)utf8_input.c_str(), (DWORD)utf8_input.length(), 0))
             {
                 DWORD hashLen = 0;
                 DWORD hashLenSize = sizeof(DWORD);
@@ -148,10 +73,93 @@ std::wstring GenerateSHA256(const std::wstring& input)
     }
     return hashStr;
 }
-LRESULT CALLBACK DashboardWndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_ALENKIN5PZ, szWindowClass, MAX_LOADSTRING);
+
+    MyRegisterClass(hInstance);
+
+    // ВАЖНО: {0} очищает мусор в оперативной памяти!
+    WNDCLASSEXW wcexDash = { 0 };
+    wcexDash.cbSize = sizeof(WNDCLASSEX);
+    wcexDash.style = CS_HREDRAW | CS_VREDRAW;
+    wcexDash.lpfnWndProc = DashboardWndProc;
+    wcexDash.cbClsExtra = 0;
+    wcexDash.cbWndExtra = 0;
+    wcexDash.hInstance = hInstance;
+    wcexDash.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ALENKIN5PZ));
+    wcexDash.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcexDash.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcexDash.lpszMenuName = NULL;
+    wcexDash.lpszClassName = szDashboardClass;
+    wcexDash.hIconSm = LoadIcon(wcexDash.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    RegisterClassExW(&wcexDash);
+
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ALENKIN5PZ));
+    MSG msg = { 0 };
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex = { 0 };
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ALENKIN5PZ));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_ALENKIN5PZ);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance;
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    // Глобальные переменные для элементов управления (чтобы считывать с них текст)
     static HWND hUsernameEdit;
     static HWND hPasswordEdit;
     static HWND hLoginButton;
@@ -160,85 +168,64 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
     {
-        // Текст "Логин"
-        CreateWindowW(L"STATIC", L"Логин:", WS_VISIBLE | WS_CHILD,
-            50, 50, 100, 20, hWnd, NULL, hInst, NULL);
-
-        // Поле ввода логина
-        hUsernameEdit = CreateWindowW(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
-            50, 70, 200, 25, hWnd, (HMENU)IDC_USERNAME_EDIT, hInst, NULL);
-
-        // Текст "Пароль"
-        CreateWindowW(L"STATIC", L"Пароль:", WS_VISIBLE | WS_CHILD,
-            50, 100, 100, 20, hWnd, NULL, hInst, NULL);
-
-        // Поле ввода пароля (с маской ES_PASSWORD для звездочек)
-        hPasswordEdit = CreateWindowW(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_PASSWORD | ES_AUTOHSCROLL,
-            50, 120, 200, 25, hWnd, (HMENU)IDC_PASSWORD_EDIT, hInst, NULL);
-
-        // Кнопка "Войти"
-        hLoginButton = CreateWindowW(L"BUTTON", L"Войти", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-            50, 160, 200, 30, hWnd, (HMENU)IDC_LOGIN_BUTTON, hInst, NULL);
-
+        CreateWindowW(L"STATIC", L"Логин:", WS_VISIBLE | WS_CHILD, 50, 50, 100, 20, hWnd, NULL, hInst, NULL);
+        hUsernameEdit = CreateWindowW(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 50, 70, 200, 25, hWnd, (HMENU)IDC_USERNAME_EDIT, hInst, NULL);
+        CreateWindowW(L"STATIC", L"Пароль:", WS_VISIBLE | WS_CHILD, 50, 100, 100, 20, hWnd, NULL, hInst, NULL);
+        hPasswordEdit = CreateWindowW(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_PASSWORD | ES_AUTOHSCROLL, 50, 120, 200, 25, hWnd, (HMENU)IDC_PASSWORD_EDIT, hInst, NULL);
+        hLoginButton = CreateWindowW(L"BUTTON", L"Войти", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 50, 160, 200, 30, hWnd, (HMENU)IDC_LOGIN_BUTTON, hInst, NULL);
         break;
     }
     case WM_COMMAND:
     {
         int wmId = LOWORD(wParam);
 
-        // Проверяем, не нажата ли наша кнопка "Войти"
         if (wmId == IDC_LOGIN_BUTTON)
         {
-            // Буферы для чтения логина и пароля
-            wchar_t username[100];
-            wchar_t password[100];
+            wchar_t username[100] = { 0 };
+            wchar_t password[100] = { 0 };
 
             GetWindowTextW(hUsernameEdit, username, 100);
             GetWindowTextW(hPasswordEdit, password, 100);
 
-            // 1. Хэшируем пароль
-            std::wstring passHash = GenerateSHA256(password);
+            if (wcslen(username) == 0 || wcslen(password) == 0) {
+                MessageBoxW(hWnd, L"Введите логин и пароль!", L"Ошибка", MB_ICONWARNING);
+                return 0;
+            }
 
-            // 2. Читаем строку подключения из config.ini
-            wchar_t connStr[256];
+            std::wstring passHash = GenerateSHA256(password);
+            wchar_t connStr[256] = { 0 };
             GetPrivateProfileStringW(L"Database", L"ConnectionString", L"", connStr, 256, L".\\config.ini");
 
-            // Собираем сообщение для проверки (временно, чтобы убедиться, что всё работает)
-            std::wstring debugMsg = L"Логин: " + std::wstring(username) +
-                L"\nХэш пароля: " + passHash +
-                L"\nСтрока БД: " + std::wstring(connStr);
-
-            // 3. Подключение к БД через ODBC и проверка пользователя
             SQLHENV hEnv = NULL;
             SQLHDBC hDbc = NULL;
             SQLHSTMT hStmt = NULL;
             SQLRETURN retCode;
 
-            // Инициализируем окружение ODBC
             SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv);
             SQLSetEnvAttr(hEnv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
             SQLAllocHandle(SQL_HANDLE_DBC, hEnv, &hDbc);
 
-            // Пытаемся подключиться по строке из config.ini
             retCode = SQLDriverConnectW(hDbc, hWnd, connStr, SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT);
 
             if (SQL_SUCCEEDED(retCode)) {
                 SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt);
-
-                // Строго прописываем нужные столбцы, никакого SELECT * 
                 wchar_t query[512];
-                swprintf_s(query, 512, L"SELECT UserID, RoleID FROM Users WHERE Username = '%s' AND PasswordHash = '%s'", username, passHash.c_str());
-
-                // Выполняем запрос
+                // Используем %ls для защиты от несовпадения типов строк
+                swprintf_s(query, 512, L"SELECT UserID, RoleID FROM Users WHERE Username = '%ls' AND PasswordHash = '%ls'", username, passHash.c_str());
                 retCode = SQLExecDirectW(hStmt, query, SQL_NTS);
 
                 if (SQL_SUCCEEDED(retCode)) {
-                    // Читаем результат (SQLFetch вернет SQL_SUCCESS, если строка найдена)
                     if (SQLFetch(hStmt) == SQL_SUCCESS) {
                         SQLINTEGER userId = 0, roleId = 0;
                         SQLLEN cbUserId = 0, cbRoleId = 0;
                         SQLGetData(hStmt, 1, SQL_C_SLONG, &userId, sizeof(userId), &cbUserId);
                         SQLGetData(hStmt, 2, SQL_C_SLONG, &roleId, sizeof(roleId), &cbRoleId);
+
+                        // Обязательно закрываем курсор чтения
+                        SQLCloseCursor(hStmt);
+
+                        // ТЕСТОВАЯ ПРОВЕРКА: вылезет окошко, подтверждающее, что база пустила
+                        MessageBoxW(hWnd, L"Доступ разрешен! Загружаю Дашборд...", L"Успех", MB_OK);
 
                         SQLHSTMT hLogStmt = NULL;
                         SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hLogStmt);
@@ -246,20 +233,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                         swprintf_s(logQuery, 512, L"INSERT INTO Sessions (UserID, IPAddress) VALUES (%d, '127.0.0.1')", userId);
                         SQLExecDirectW(hLogStmt, logQuery, SQL_NTS);
                         SQLFreeHandle(SQL_HANDLE_STMT, hLogStmt);
-
-                        MessageBoxW(hWnd, L"Доступ разрешен. Вход записан в системный журнал.", L"Успех", MB_OK);
-
-                        // 2. Скрываем окно авторизации
-                        ShowWindow(hWnd, SW_HIDE);
-
-                        // 3. Создаем и показываем форму Дашборда
+                        
                         HWND hDash = CreateWindowW(szDashboardClass, L"Главный дашборд - Панель управления",
-                            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                            WS_OVERLAPPEDWINDOW,
                             CW_USEDEFAULT, 0, 800, 600, nullptr, nullptr, hInst, nullptr);
 
-                        // Заготовка на будущее:
-                        // 1. Сделать запись в SystemLogs (ID сессии и IP) [cite: 20]
-                        // 2. Скрыть это окно и открыть форму "Главный дашборд" [cite: 7]
+                        if (hDash) {
+                            ShowWindow(hDash, SW_SHOW);
+                            UpdateWindow(hDash);
+
+                            ShowWindow(hWnd, SW_HIDE);
+                        }
+                        else {
+                            MessageBoxW(NULL, L"Окно дашборда не смогло открыться!", L"Критическая ошибка", MB_ICONERROR);
+                            PostQuitMessage(1);
+                        }
                     }
                     else {
                         MessageBoxW(hWnd, L"Неверный логин или пароль!", L"Отказ в доступе", MB_ICONWARNING);
@@ -268,21 +256,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 else {
                     MessageBoxW(hWnd, L"Ошибка выполнения SQL-запроса.", L"Ошибка БД", MB_ICONERROR);
                 }
-
-                // Освобождаем память запроса и отключаемся
+                // Полностью освобождаем память запроса
                 SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
                 SQLDisconnect(hDbc);
             }
             else {
-                MessageBoxW(hWnd, L"Не удалось подключиться к серверу БД. Проверь config.ini", L"Ошибка сети", MB_ICONERROR);
+                MessageBoxW(hWnd, L"Не удалось подключиться к БД. Проверь config.ini", L"Ошибка сети", MB_ICONERROR);
             }
-
-            // Убиваем дескрипторы
             SQLFreeHandle(SQL_HANDLE_DBC, hDbc);
             SQLFreeHandle(SQL_HANDLE_ENV, hEnv);
+
+            return 0;
         }
 
-        // Разобрать стандартный выбор в меню:
         switch (wmId)
         {
         case IDM_ABOUT:
@@ -312,7 +298,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// Обработчик сообщений для окна "О программе".
+// Обработчик окна Главного Дашборда
+LRESULT CALLBACK DashboardWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        TextOutW(hdc, 50, 50, L"Добро пожаловать в систему управления производством!", 52);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
@@ -330,27 +337,4 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
-
-}
-// Обработчик окна Главного Дашборда
-LRESULT CALLBACK DashboardWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
-        // Выведем текст приветствия на новой форме
-        TextOutW(hdc, 50, 50, L"Добро пожаловать в систему управления производством!", 52);
-        EndPaint(hWnd, &ps);
-    }
-    break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
 }
