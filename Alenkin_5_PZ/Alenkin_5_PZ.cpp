89675a4efd719b4d62cd3d49d9da73ba420136fe
tanks8015-hue <tanks8015@gmail.com>
2026-04-02 13:47:55 +0400

    добавлен Справочник материалов (работает корректно)

--- Alenkin_5_PZ/Alenkin_5_PZ.cpp
+++ Alenkin_5_PZ/Alenkin_5_PZ.cpp
@@ -808,6 +808,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     UNREFERENCED_PARAMETER(lParam);
     switch (message)
     {
+
     case WM_INITDIALOG: return (INT_PTR)TRUE;
     case WM_COMMAND:
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
